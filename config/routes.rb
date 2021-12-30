35458df60934549547f772984f38060ae54894f7
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-12-30 16:48:15 +0200

    Fix logout error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  resources :sessions, only: %i[new create logout_user]
+  delete '/logout', to: 'sessions#logout_user'
+  resources :sessions, only: %i[new create]
   resources :posts
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
