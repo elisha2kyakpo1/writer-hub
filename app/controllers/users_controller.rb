b8ecc1f35cc2f038c8b3f15d0e40bc70fc6575e0
elisha2kyakpo1 <kyakopoe@gmail.com>
2022-01-01 12:27:25 +0200

    Add rich text to post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_action :require_login, only: %i[create]
   def index
     @users = User.order('created_at DESC')
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  has_rich_text :body
   belongs_to :author, class_name: 'User'
   validates :name, presence: true
 end
